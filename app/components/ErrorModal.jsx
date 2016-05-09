ef963200528db0a0cb982c2a2b8f33f6a7603414
Mihir Karandikar <Mihir@Mihir-Mac-mini.local>
2016-05-09 11:51:01 +0530

    fixed modal error

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,25 +1,36 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 module.exports = React.createClass({
-  propTypes: {
-    title: React.PropTypes.string,
-    message: React.PropTypes.string.isRequired
-  },
   getDefaultProps: function(){
     return{
       title: 'Error'
     }
   },
+  propTypes: {
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired
+  },
   componentDidMount: function(){
-    var modal =new Foundation.Reveal($('#error-modal'));
+    var {title, message} = this.props;
+    var modalMarkup = (
+      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+          <h4>{title}</h4>
+          <p>{message}</p>
+          <p><button className="button hollow" data-close="">Okay</button></p>
+      </div>
+    );
+
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    var modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
   },
   render: function(){
     return(
-      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-          <h4>{this.props.title}</h4>
-          <p>{this.props.message}</p>
-          <p><button className="button hollow" data-close="">Okay</button></p>
+      <div>
       </div>
     );
   }
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -39,7 +39,7 @@ module.exports = React.createClass({
 
     function renderErrorModal(){
       if(typeof errorMessage === "string"){
-        return <ErrorModal message={errorMessage} />;
+        return <ErrorModal title={'Something went wrong'} message={errorMessage} />;
       }
     }
 
